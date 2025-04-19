4faea5465a71172b02a89cf62505beee3600d17a
MTavaresBorges <matheustavaresborges2000@gmail.com>
2025-04-19 17:35:08 -0300

    first commit

---
+++ database/migrations/2025_04_19_195506_create_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('state_id')->constrained('states')->onDelete('cascade');
+            $table->string('street');
+            $table->string('complement');
+            $table->string('number');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('cep');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
